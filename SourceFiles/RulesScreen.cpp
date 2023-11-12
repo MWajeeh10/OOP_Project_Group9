03443486ce8eabe2278ecf5d18af733bc945b27b
MWajeeh10 <mw08325@st.habib.edu.pk>
2023-11-12 23:00:54 +0500

    rulesscreen changes

--- SourceFiles/RulesScreen.cpp
+++ SourceFiles/RulesScreen.cpp
@@ -1,19 +1,37 @@
 #include "RulesScreen.hpp"
-#include <SDL.h>
-#include <SDL_ttf.h>
+#include <SDL_image.h>
 
-RulesScreen::RulesScreen() {
+
+RulesScreen::RulesScreen(SDL_Renderer* renderer): renderer(renderer){
     // Initialize SDL and TTF here
+    loadRulesTexture(); //loads the rules image
 }
 
-void RulesScreen::handleEvents() {
+RulesScreen::~RulesScreen(){
+    SDL_DestroyTexture(rulesTexture);  //frees memory allocated for rulesTexture
+}
+
+void RulesScreen::handleEvents(SDL_Events& event) {
     // Handle mouse events, implement navigation
+    if(event.type == SDL_MOUSEBUTTONDOWN){
+        //check if the click is within the back button's rectangle
+        int mouseX, mouseY;  //current coordinates of mouse pointer on screen
+        SDL_GetMouseState(&mouseX, &mouseY); //retrieve the above values
+
+        if (mouseX >= backButtonRect.x && mouseX <= backButtonRect.x + backButtonRect.w 
+            && mouseY >= backButtonRect.y && mouseY <= backButtonRect.y + backButtonRect.h)
+            {
+                handleBackButtonClick();
+            }
+    }
 }
 
 void RulesScreen::update() {
-    // Update logic if needed
+    // will have to update or implement any logic if required. otherwise no need to modify it
 }
 
 void RulesScreen::render() {
     // Render rules text and back button
+    SDL_RenderCopy(renderer, rulesTexture, nullptr, nullptr);
+    renderBackButton();
 }
